d3c4ff14300321a88036175d4e5f1ba4fcec2b20
Anastasia <anastasiavlg@mail.ru>
2021-11-08 12:32:28 +0300

    Make bot display information about itself

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,7 +1,11 @@
 package bot;
 
 public class SimpleBot {
+    private static final String BOT_NAME = "Anfisa";
+    private static final int CREATION_YEAR = 2021;
+
     public static void main(String[] args) {
-        // write your code here
+        System.out.printf("Hello! My name is %s.\n", BOT_NAME);
+        System.out.printf("I was created in %d.\n", CREATION_YEAR);
     }
 }
