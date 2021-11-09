942ca5180b2cd10975c60bcbd63fd391105018c9
Anastasia <anastasiavlg@mail.ru>
2021-11-09 19:18:11 +0300

    Make bot ask and print user name

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,5 +1,7 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     private static final String BOT_NAME = "Anfisa";
     private static final int CREATION_YEAR = 2021;
@@ -7,5 +9,12 @@ public class SimpleBot {
     public static void main(String[] args) {
         System.out.printf("Hello! My name is %s.\n", BOT_NAME);
         System.out.printf("I was created in %d.\n", CREATION_YEAR);
+        System.out.println("Please, remind me your name.");
+
+        // reading a name
+        final var scanner = new Scanner(System.in);
+        final var name = scanner.nextLine();
+
+        System.out.printf("What a great name you have, %s!\n", name);
     }
 }
